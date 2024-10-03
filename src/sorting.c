c529e08e7735ce3138c5c37ae4fe667e11453a12
Alex Kling <mkling@student.42.fr>
2024-10-03 11:30:34 +0200

    cleanup

--- src/sorting.c
+++ src/sorting.c
@@ -6,7 +6,7 @@
 /*   By: mkling <mkling@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 10:49:47 by mkling            #+#    #+#             */
-/*   Updated: 2024/10/03 10:48:55 by mkling           ###   ########.fr       */
+/*   Updated: 2024/10/03 11:28:35 by mkling           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,33 +23,33 @@ static int	cost_to_top(t_dlst *node, int stack_len)
 	return (cost);
 }
 
-static void	opti_double_rota(t_dlst *a, int len_a, int len_b)
-{
-	int		rrr_cost;
-	int		rrev_cost;
-	t_dlst	*b;
+// static void	opti_double_rota(t_dlst *a, int len_a, int len_b)
+// {
+// 	int		rrr_cost;
+// 	int		rrev_cost;
+// 	t_dlst	*b;
 
-	a->opti = NONE;
-	b = a->target;
-	rrr_cost = ft_min(a->index, b->index)
-		+ (ft_max(a->index, b->index) - ft_min(a->index, b->index));
-	if ((len_b - b->index) > (len_a - a->index))
-		rrev_cost = (len_a - a->index) + ((len_b - b->index)
-				- (len_a - a->index));
-	if ((len_b - b->index) < (len_a - a->index))
-		rrev_cost = (len_b - b->index) + ((len_a - a->index)
-				- (len_b - b->index));
-	if ((rrr_cost < a->push_cost + 1))
-	{
-		a->push_cost = rrr_cost + 1;
-		a->opti = ROT_UP;
-	}
-	if ((rrev_cost < a->push_cost + 1))
-	{
-		a->push_cost = rrev_cost + 1;
-		a->opti = REV_DO;
-	}
-}
+// 	a->opti = NONE;
+// 	b = a->target;
+// 	rrr_cost = ft_min(a->index, b->index)
+// 		+ (ft_max(a->index, b->index) - ft_min(a->index, b->index));
+// 	if ((len_b - b->index) > (len_a - a->index))
+// 		rrev_cost = (len_a - a->index) + ((len_b - b->index)
+// 				- (len_a - a->index));
+// 	if ((len_b - b->index) < (len_a - a->index))
+// 		rrev_cost = (len_b - b->index) + ((len_a - a->index)
+// 				- (len_b - b->index));
+// 	if ((rrr_cost < a->push_cost + 1))
+// 	{
+// 		a->push_cost = rrr_cost + 1;
+// 		a->opti = ROT_UP;
+// 	}
+// 	if ((rrev_cost < a->push_cost + 1))
+// 	{
+// 		a->push_cost = rrev_cost + 1;
+// 		a->opti = REV_DO;
+// 	}
+// }
 
 void	set_push_cost(t_dlst *src, t_dlst *dest)
 {
@@ -108,23 +108,23 @@ void	push_cheapest(t_dlst **src, t_dlst **dest)
 	push_top(src, dest);
 }
 
-void	mecha_turk_sort(t_dlst **stack_a, t_dlst **stack_b)
+void	mecha_turk_sort(t_dlst **a, t_dlst **b)
 {
-	while (stack_len(*stack_a) > 3 && stack_len(*stack_b) < 2)
-		push_top(stack_a, stack_b);
-	while (stack_len(*stack_a) > 3)
+	while (stack_len(*a) > 3 && stack_len(*b) < 2)
+		push_top(a, b);
+	while (stack_len(*a) > 3)
 	{
-		set_index(*stack_a, *stack_b);
-		set_target_closest_smaller(*stack_a, *stack_b);
-		set_push_cost(*stack_a, *stack_b);
-		push_cheapest(stack_a, stack_b);
+		set_index(*a, *b);
+		set_target_closest_smaller(*a, *b);
+		set_push_cost(*a, *b);
+		push_cheapest(a, b);
 	}
-	tiny_sort_to_nearest_rotation(stack_a);
-	while (stack_len(*stack_b) > 0)
+	tiny_sort_to_nearest_rotation(a);
+	while (stack_len(*b) > 0)
 	{
-		set_index(*stack_a, *stack_b);
-		set_target_closest_bigger(*stack_b, *stack_a);
-		push_cheapest(stack_b, stack_a);
+		set_index(*a, *b);
+		set_target_closest_bigger(*b, *a);
+		push_cheapest(b, a);
 	}
-	rotate_to_top(stack_a, find_smallest_num_in_stack(*stack_a));
+	rotate_to_top(a, find_smallest_num_in_stack(*a));
 }
