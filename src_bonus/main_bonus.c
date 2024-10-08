4835fb99913a94d18305cf69c6bb33383be66f6b
Alex Kling <mkling@student.42.fr>
2024-10-08 15:01:23 +0200

    Corrected

--- src_bonus/main_bonus.c
+++ src_bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mkling <mkling@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 00:56:07 by mkling            #+#    #+#             */
-/*   Updated: 2024/10/08 14:38:35 by mkling           ###   ########.fr       */
+/*   Updated: 2024/10/08 14:57:14 by mkling           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,28 +40,6 @@ int	main(int argc, char **argv)
 	return (0);
 }
 
-void	print_stack(t_dlst **stack)
-{
-	t_dlst	*current;
-
-	if (!(*stack))
-		return ;
-	current = (*stack);
-	fprintf(stderr, "printing stack %c\n", (*stack)->stack_id);
-	while (current != NULL)
-	{
-		fprintf(stderr, "node = %d\n", current->data);
-		current = current->next;
-	}
-}
-
-void	print_both_stacks(t_dlst **a, t_dlst **b)
-{
-	fprintf(stderr, "\nPRINT\n");
-	print_stack(a);
-	print_stack(b);
-}
-
 int	stack_len(t_dlst *stack)
 {
 	int	stack_len;
@@ -75,3 +53,24 @@ int	stack_len(t_dlst *stack)
 	return (stack_len);
 }
 
+// void	print_stack(t_dlst **stack)
+// {
+// 	t_dlst	*current;
+
+// 	if (!(*stack))
+// 		return ;
+// 	current = (*stack);
+// 	fprintf(stderr, "printing stack %c\n", (*stack)->stack_id);
+// 	while (current != NULL)
+// 	{
+// 		fprintf(stderr, "node = %d\n", current->data);
+// 		current = current->next;
+// 	}
+// }
+
+// void	print_both_stacks(t_dlst **a, t_dlst **b)
+// {
+// 	fprintf(stderr, "\nPRINT\n");
+// 	print_stack(a);
+// 	print_stack(b);
+// }
